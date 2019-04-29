ef73460bc2c82fb854b01f51d077b878571c9216
Lukas Lücke <lukas@luecke.me>
2019-04-29 13:39:16 +0200

    fix: don't extract fields for normalization

--- src/GraphQl/Resolver/Factory/ItemMutationResolverFactory.php
+++ src/GraphQl/Resolver/Factory/ItemMutationResolverFactory.php
@@ -82,7 +82,7 @@ final class ItemMutationResolverFactory implements ResolverFactoryInterface
             $resourceMetadata = $this->resourceMetadataFactory->create($resourceClass);
             $wrapFieldName = lcfirst($resourceMetadata->getShortName());
             $normalizationContext = $resourceMetadata->getGraphqlAttribute($operationName ?? '', 'normalization_context', [], true);
-            $normalizationContext['attributes'] = $this->fieldsToAttributes($info)[$wrapFieldName] ?? [];
+            $normalizationContext['attributes'] = $this->fieldsToAttributes($info)[$wrapFieldName] ?? $this->fieldsToAttributes($info);;
 
             if (isset($args['input']['id'])) {
                 try {
