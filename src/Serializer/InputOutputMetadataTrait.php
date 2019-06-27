06254c6d2758b3d02c906126ee63f3db28a2608c
Sergey Logachev <serg@baitek.org>
2019-06-27 15:28:54 +0300

    Fix for DTO on operation level

--- src/Serializer/InputOutputMetadataTrait.php
+++ src/Serializer/InputOutputMetadataTrait.php
@@ -45,6 +45,10 @@ trait InputOutputMetadataTrait
             return null;
         }
 
-	    return $metadata->getGraphqlAttribute($context['graphql_operation_name'], $inputOrOutput)['class'] ?? $metadata->getAttribute($inputOrOutput)['class'] ?? null;
+        $graphQlAttribute = isset($context['graphql_operation_name'])
+	        ? $metadata->getGraphqlAttribute($context['graphql_operation_name'], $inputOrOutput)
+	        : null;
+
+	    return $graphQlAttribute['class'] ?? $metadata->getAttribute($inputOrOutput)['class'] ?? null;
     }
 }
